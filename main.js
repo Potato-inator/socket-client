793e5167a6d11dc9f8f15166253f5e9c5e8377c2
Isaiah Rose <expert4minecraft02@gmail.com>
2023-06-06 09:07:26 -0400

    Final Commit (for now)

--- main.js
+++ main.js
@@ -1,31 +1,41 @@
 import './style.css'
 import { io } from "socket.io-client";
-import { changeName } from './modules/change-name';
 
-const socket = io("ws://localhost:3000");
+//Using .env to input a host IP 
+const socket = io(import.meta.env.VITE_IP);
 const clickBTN = document.getElementById('sendClick');
 const nameBTN = document.getElementById('changeName');
 const resetBTN = document.getElementById('resetClicks');
 var nameInput = document.getElementById('nameInput');
 
+//Button Listeners
 clickBTN.addEventListener('click', clicked);
 nameBTN.addEventListener('click', changeName);
 resetBTN.addEventListener('click', resetClicks);
 
-changeName(socket, nameInput.value);
+function changeName() {
+  var data = { name: nameInput.value }
+  socket.emit("changeName", data);
+}
 
-function clicked(socket) {
+function clicked() {
   socket.emit("click");
 }
 
-function resetClicks(socket) {
+function resetClicks() {
   socket.emit("resetClicks");
 }
 
+//If someone clicks
 socket.on("someoneClicked", (data) => {
   document.getElementById('totalClicks').innerHTML = data.totalClicks
 })
   
+//If someone resets
 socket.on("someoneResetClicks", (data) => {
   document.getElementById('totalClicks').innerHTML = data.totalClicks
+})
+
+socket.on("connectComplete",() => {
+  console.log("Successful connection.")
 })
--- modules/change-name.js
+++
@@ -1,5 +0,0 @@
-export function changeName(socket, nameInput) {
-  var data = { name: nameInput}
-  socket.emit("changeName", data);
-  alert("You changed your name to: " + nameInput)
-}
